cbf7d58fa6ca4cbd49ac32baa66909b35d591528
xiaozhang <xiaozhang@freewheel.tv>
2017-11-07 18:54:48 +0800

    fix issue #69: outdated jobs are executed all at once when pool is started

--- worker.go
+++ worker.go
@@ -190,10 +190,16 @@ func (w *worker) processJob(job *Job) {
 		}
 	}()
 	if jt, ok := w.jobTypes[job.Name]; ok {
+		if jt.StartingDeadline > 0 && job.ScheduledAt > 0 && job.ScheduledAt < jt.StartingDeadline {
+			logInfo("Outdated! StartingDeadline: %d, ScheduledAt: %d, Job: %s\n", jt.StartingDeadline, job.ScheduledAt, job)
+			w.removeJobFromInProgress(job)
+			return
+		}
 		w.observeStarted(job.Name, job.ID, job.Args)
 		job.observer = w.observer // for Checkin
 		_, runErr := runJob(job, w.contextType, w.middleware, jt)
 		w.observeDone(job.Name, job.ID, runErr)
+
 		if runErr != nil {
 			job.failed(runErr)
 			w.addToRetryOrDead(jt, job, runErr)
